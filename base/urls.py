f722de58ff8c5c0bfad374257f7b169e1ac68b11
biszaal <biszaal@yahoo.com>
2022-06-21 09:41:34 +0545

    first commit

---
+++ base/urls.py
@@ -0,0 +1,11 @@
+from turtle import home
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+]
