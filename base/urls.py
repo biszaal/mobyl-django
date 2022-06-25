4228cc54b09fdec0107230e2d895562f4c614552
biszaal <biszaal@yahoo.com>
2022-06-25 10:01:45 +0545

    added home

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('login/', views.loginPage, name="login"),
+    path('', views.home, name='home'),
+    path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
 ]
